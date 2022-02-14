2c914f2881caee14dc39212b318638fa982fb297
Edson Caliman <edsoncaliman@gmail.com>
2022-02-14 07:18:14 -0300

    Solution, folders and initial projects

---
+++ Banking.Operation.Receipt.Consumer.Worker/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Banking.Operation.Receipt.Consumer.CrossCutting.Ioc;
+using System.IO;
+
+namespace Banking.Operation.Receipt.Consumer.Worker
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = ReturnEnvironment(config);
+                    config
+                         .SetBasePath(Directory.GetCurrentDirectory())
+                         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                         .AddJsonFile($"appsettings.{(string.IsNullOrEmpty(env) ? "Development" : env)}.json", optional: true, reloadOnChange: true)
+                         .AddEnvironmentVariables();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                    services.ConfigureContainer(hostContext.Configuration);
+                });
+
+        private static string ReturnEnvironment(IConfigurationBuilder configBuilder)
+        => configBuilder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build()
+            .GetSection("Environment")?.Value;
+    }
+}
